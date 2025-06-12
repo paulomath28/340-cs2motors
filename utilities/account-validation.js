36fb19492102058ee59a5121b84e9b7794e41631
Paulo André <67445880+paulomath28@users.noreply.github.com>
2025-06-12 18:54:43 -0300

    Add files via upload

---
+++ utilities/account-validation.js
@@ -0,0 +1,53 @@
+const utilities = require(".");
+const { body, validationResult } = require('express-validator');
+const validate = {}
+const accountModel = require("../models/account-model")
+
+/*  **********************************
+  *  Registration Data Validation Rules
+  * ********************************* */
+validate.registrationRules = () => {
+    return [
+      // firstname is required and must be string
+      body("account_firstname")
+        .trim()
+        .escape()
+        .notEmpty()
+        .isLength({ min: 1 })
+        .withMessage("Please provide a first name."), // on error this message is sent.
+  
+      // lconst utilities = require(".");
+const { body, validationResult } = require('express-validator');
+const validate = {}
+const accountModel = require("../models/account-model")
+
+/*  **********************************
+  *  Registration Data Validation Rules
+  * ********************************* */
+validate.registrationRules = () => {
+    return [
+      // firstname is required and must be string
+      body("account_firstname")
+        .trim()
+        .escape()
+        .notEmpty()
+        .isLength({ min: 1 })
+        .withMessage("Please provide a first name."), // on error this message is sent.
+  
+      // lastname is required and must be string
+      body("account_lastname")
+        .trim()
+        .escape()
+        .notEmpty()
+        .isLength({ min: 2 })
+        .withMessage("Please provide a last name."), // on error this message is sent.
+  
+      // valid email is required and cannot already exist in theastname is required and must be string
+      body("account_lastname")
+        .trim()
+        .escape()
+        .notEmpty()
+        .isLength({ min: 2 })
+        .withMessage("Please provide a last name."), // on error this message is sent.
+  
+      // valid email is required and cannot already exist in the
